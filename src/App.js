c055c5c06257dcd09ff84a6162ed7fba46c46994
imisrafil <im.israfil.fi@gmail.com>
2023-06-13 17:32:51 +0600

    Learn to use react useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useMemo, useCallback } from "react";
 
-function App() {
+const getArry = () => {
+  for (let i = 0; i <= 1000000000; i++) {
+    i++;
+  }
+
+  return ["Israfil", "Mallick"];
+};
+
+const App = () => {
+  const [userNumber, setUserNumber] = useState("");
+  const [randomInput, setRandomInput] = useState("");
+
+  const fib = useCallback((n) => {
+    if (isNaN(n) || typeof n !== "number") return "Please enter a Valid Number";
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  }, []);
+
+  const fibNumber = useMemo(() => fib(userNumber), [userNumber, fib]);
+  // const fibNumber = fib(userNumber);
+  const array = useMemo(() => getArry(), []);
+
+  useEffect(() => {
+    // console.log("fibNumber changing");
+    console.log("array changing");
+  }, [array]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="number">Number input</label>
+      <input
+        type="text"
+        id="number"
+        value={userNumber}
+        onChange={(e) => setUserNumber(parseInt(e.target.value) || 0)}
+      />
+      <p>{fibNumber}</p>
+      <br />
+      <label htmlFor="random">Ramdom Input</label>
+      <input
+        type="text"
+        id="random"
+        value={randomInput}
+        onChange={(e) => setRandomInput(e.target.value)}
+      />
     </div>
   );
-}
+};
 
 export default App;
